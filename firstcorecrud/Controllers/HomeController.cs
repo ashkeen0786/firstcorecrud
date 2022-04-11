519d2fbaa6430292072ebc7b88ac0755567f48bc
ashkeen <ansariashkeen44@gmail.com>
2022-04-11 12:17:17 -0700

    Add project files.

---
+++ firstcorecrud/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using firstcorecrud.db;
+using firstcorecrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firstcorecrud.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            coredbContext dbpbj = new coredbContext();
+            var show = dbpbj.Coretablees.ToList();
+            return View(show);
+        }
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task< IActionResult> Privacy(student clobj)
+        {
+            coredbContext dbpbj = new coredbContext();
+            Coretablee tbobj = new Coretablee();
+            tbobj.Id = clobj.Id;
+            tbobj.Name = clobj.Name;
+            tbobj.Email = clobj.Email;
+            tbobj.Phone = clobj.Phone;
+            if (clobj.Id == 0) 
+            {
+                dbpbj.Coretablees.Add(tbobj);
+                dbpbj.SaveChanges();
+            }
+            else
+            {
+                dbpbj.Update(tbobj);
+               // dbpbj.Entry(clobj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                dbpbj.SaveChanges();
+            }
+            return RedirectToAction("Index");
+            
+
+        }
+
+
+
+        public IActionResult delete(int id)
+        {
+            coredbContext dbpbj = new coredbContext();
+            var del = dbpbj.Coretablees.Where(m => m.Id == id).First();
+            dbpbj.Coretablees.Remove(del);
+            dbpbj.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult edit(int id)
+        {
+            coredbContext dbpbj = new coredbContext();
+            var edi = dbpbj.Coretablees.Where(m => m.Id == id).First();
+            student obj = new student();
+            obj.Id = edi.Id;
+            obj.Name = edi.Name;
+            obj.Email = edi.Email;
+            obj.Phone = edi.Phone;
+            return View("privacy",obj);
+        }
+
+            //    public IActionResult Privacy()
+            //{
+            //    return View();
+            //}
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ firstcorecrud/db/coredbContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace firstcorecrud.db
+{
+    public partial class coredbContext : DbContext
+    {
+        public coredbContext()
+        {
+        }
+
+        public coredbContext(DbContextOptions<coredbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Coretablee> Coretablees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-3I7N9AP\\SQLEXPRESS;Database=coredb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coretablee>(entity =>
+            {
+                entity.ToTable("coretablee");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("phone");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
